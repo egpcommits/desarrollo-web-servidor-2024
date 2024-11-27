24360a849dde814931153d5c73d872a36cd6a4e9
egpcommits <181844308+egpcommits@users.noreply.github.com>
2024-11-27 13:33:11 +0100

    Corrigiendo login de animes - 27 de noviembre

--- 06_bases_de_datos/animes/usuario/registro.php
+++ 06_bases_de_datos/animes/usuario/registro.php
@@ -42,7 +42,7 @@
             </div>
         </form>
         <h3>0, si ya tienes cuenta, inicia sesión.</h3>   
-        <a class="btn btn-secondary" href="../iniciar_sesion.php">Iniciar sesión</a>
+        <a class="btn btn-secondary" href="iniciar_sesion.php">Iniciar sesión</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
