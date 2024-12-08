61e95936964c6c02e876a9a3678b1adc60dd83b8
egpcommits <181844308+egpcommits@users.noreply.github.com>
2024-12-08 20:50:58 +0100

    Practica tienda finalizada - 8 de diciembre

--- 06_bases_de_datos/tienda/util/script_tienda.sql
+++ 06_bases_de_datos/tienda/util/script_tienda.sql
@@ -1,15 +1,13 @@
 CREATE SCHEMA tienda_bd;
 USE tienda_bd;
 
-DELETE FROM usuarios WHERE usuario = "estela";
-
 CREATE TABLE usuarios (
 	usuario VARCHAR(15) PRIMARY KEY,
     contrasena VARCHAR(255) NULL
 );
 
 CREATE TABLE categorias (
-	nombre VARCHAR(30) PRIMARY KEY,
+	categoria VARCHAR(30) PRIMARY KEY,
     descripcion VARCHAR(255)
 );
 
@@ -21,7 +19,7 @@ CREATE TABLE productos (
     stock INT(3) DEFAULT 0,
     imagen VARCHAR(60),
     descripcion VARCHAR(255),
-    CONSTRAINT fk_productos_categoria FOREIGN KEY (categoria) REFERENCES categorias(nombre) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_productos_categoria FOREIGN KEY (categoria) REFERENCES categorias(categoria) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 SET sql_safe_updates = 0;
