38b0219db5d0239aedc786e73b684a06eaac55a0
egpcommits <181844308+egpcommits@users.noreply.github.com>
2025-01-10 12:58:35 +0100

    Empezando API - 10 de enero

---
+++ 06_bases_de_datos/animes/api/api_estudios.php
@@ -0,0 +1,27 @@
+<?php
+    error_reporting( E_ALL );
+    ini_set( "display_errors", 1 );
+
+    header("Content-type: application/json");
+    include("conexion_pdo.php");
+
+    $metodo = $_SERVER["REQUEST_METHOD"];
+    $entrada = json_decode(file_get_contents('php://input'), true); //almacena en entrada los parametros
+
+    switch($metodo) {
+        case "GET":
+            manejarGet($_conexion);
+            break;
+        case "POST":
+            manejarPost($_conexion, $entrada);
+            break;
+        case "PUT":
+            manejarPut($_conexion, $entrada);
+            break;
+        case "DELETE":
+            manejarDelete($_conexion, $entrada);
+            break;
+        default:
+            echo json_encode(["mensaje" => "Petición no válida."]);
+    }
+?>
