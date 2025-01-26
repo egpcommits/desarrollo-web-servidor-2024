dad2c0bad6e736fb6ef7322e0e37ffea555ea839
egpcommits <181844308+egpcommits@users.noreply.github.com>
2025-01-26 23:02:19 +0100

    Acabado top anime con API - 26 de enero

--- 07_Apis/Jikan/anime.php
+++ 07_Apis/Jikan/anime.php
@@ -4,12 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anime</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="bootstrap.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-size: 1.15rem;
+            text-align: justify;
+            font-family: Ubuntu;
+        }
+
+        img {
+            border: 1px solid white;
+        }
+
+        h1 {
+            text-shadow: 2px 2px #00bc8c;
+        }
+    </style>
+    <?php
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );
+    ?>
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <?php
-        $url = "https://api.jikan.moe/v4/top/anime";
+
+        $id = intval($_GET["mal_id"]);
+
+        //rint_r(var_dump($id));
+
+        $url = "https://api.jikan.moe/v4/anime/" . $id;
 
         $curl = curl_init(); //inicia el curl
         curl_setopt($curl, CURLOPT_URL, $url); //se le pasa la url
@@ -20,14 +47,51 @@
         $datos = json_decode($respuesta, true); //para pasar de json a array
         $animes = $datos["data"]; //se accede al apartado data del json
 
-        $id = $_GET["mal_id"];
+        
     ?>
 
     <div class="container">
+        <div class="row mt-5">
+            <div class="col">
+                <a class="btn btn-success btn-sm" href="top_anime.php">Volver</a>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <h1 class="text-body"><?php echo $animes["title"] ?></h1>
+            </div>
+            <div class="col-1 offset">
+                <h1><?php echo $animes["score"] ?></h1>
+            </div>
+        </div>        
+        <div class="row mt-5">
+            <div class="justify-content-center d-flex col-3">
+                <img src="<?php echo $animes['images']['jpg']['image_url'] ?>">
+            </div>
+            <div class="col">
+                <p><?php echo $animes["synopsis"] ?></p>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-3">
+                <p>Año de estreno: <?php echo $animes["year"] ?></p>
+            </div>
+        </div>
         <div class="row">
-            <?php foreach($animes as $anime) { ?>
-                <h1><?php $anime["title"] ?></h1>
-            <?php } ?>
+            <div class="col-3">
+                <p>Estudio: <?php echo $animes["studios"]["0"]["name"] ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <p>Número de episodios: <?php echo $animes["episodes"] ?></p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col offset-4 mt-5 mb-5">
+                <iframe width="600" height="350" src="<?php echo $animes['trailer']['embed_url'] ?>" allowfullscreen></iframe>
+            </div>
+            
         </div>
     </div>
 </body>
--- 07_Apis/Jikan/top_anime.php
+++ 07_Apis/Jikan/top_anime.php
@@ -4,12 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Anime</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <?php
         error_reporting( E_ALL );
         ini_set( "display_errors", 1 );
     ?>
 </head>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+        * {
+            font-size: 1.15rem;
+            text-align: justify;
+            font-family: Ubuntu;
+        }
+
+        img {
+            border: 1px solid white;
+        }
+
+        h1 {
+            text-shadow: 2px 2px #00bc8c;
+        }
+    </style>
     <?php
         $url = "https://api.jikan.moe/v4/top/anime";
 
@@ -24,28 +44,32 @@
 
         //print_r($animes);
     ?>
-
-    <table>
-        <thead>
-            <tr>
-                <th>Titulo</th>
-                <th>Nota</th>
-                <th>Imagen</th>
-                <th>Ver más</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($animes as $anime) { ?>
-                    <tr>
-                        <td><?php echo $anime["title"] ?></td>
-                        <td><?php echo $anime["score"] ?></td>
-                        <td><img src="<?php echo $anime["images"]["jpg"]["image_url"] ?>"></td>
-                        <td><a href="anime.php?=<?php echo $anime["mal_id"] ?>">Enlace</a></td>
-                    </tr>
-            <?php } ?>
-        </tbody>
-        
-    </table>
+    <div class="container">
+        <h1 class="mt-5 text-center">TOP ANIME</h1>
+        <table class="table table-hover table-striped mt-5 align-middle">
+            <thead>
+                <tr class="text-center">
+                    <th scope="col">Rank</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Nota</th>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Ver más información</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($animes as $anime) { ?>
+                        <tr>
+                            <td class="text-center"><?php echo $anime["rank"] ?></td>
+                            <td><?php echo $anime["title"] ?></td>
+                            <td class="text-center"><?php echo $anime["score"] ?></td>
+                            <td class="text-center"><img src="<?php echo $anime["images"]["jpg"]["image_url"] ?>"></td>
+                            <td class="text-center"><a class="btn btn-success btn-sm" href="anime.php?mal_id=<?php echo $anime['mal_id'] ?>">Enlace</a></td>
+                        </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    
 </body>
 </html>
